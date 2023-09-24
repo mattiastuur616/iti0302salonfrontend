3b4643235e5e1d3bcf041f351e02d7367c951603
Mattias Tüür <mattiastuur@gmail.com>
2023-09-24 15:14:01 +0300

    Added form.

--- src/components/RegisterServ.vue
+++ src/components/RegisterServ.vue
@@ -1,21 +1,56 @@
 <template>
   <HeaderFile />
   <h1 class="RegisterHeader">Here You can register different Services from our Salon.</h1>
+  <table border="1" align="center">
+    <tr>
+      <td>ID</td>
+      <td>Name</td>
+      <td>Price</td>
+      <td>Duration</td>
+      <td>Date</td>
+    </tr>
+    <tr v-for="item in services" :key="item.id">
+      <td>{{item.serviceId}}</td>
+      <td>{{item.name}}</td>
+      <td>{{item.price}}</td>
+      <td>{{item.duration}} minutes</td>
+      <td>{{item.startingTime}}</td>
+    </tr>
+  </table>
 </template>
 <script>
 import HeaderFile from "@/components/HeaderFile.vue";
+import axios from 'axios';
 export default {
   name:'RegisterServ',
+  data(){
+    return {
+      name:'',
+      services:[]
+    }
+  },
   components:{
     HeaderFile
   },
-  mounted()
+  async mounted()
   {
     let user = localStorage.getItem('user-info');
     if(!user)
     {
       this.$router.push({name:'SignUp'})
     }
+    let result = await axios.get('http://localhost:3000/services');
+    console.warn(result)
+    this.services=result.data;
   }
 }
 </script>
+
+<style>
+td{
+  width:160px;
+  height:40px;
+  text-align: center;
+  font-size: 20px;
+}
+</style>
